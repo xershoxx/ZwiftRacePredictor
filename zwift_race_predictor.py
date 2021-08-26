2c747f9c0298a307fd2f90d16b745359c5988c75
Claudio Homeyer <xershoxx@users.noreply.github.com>
2021-08-26 21:57:25 +0200

    new empty function for processing rider data

--- zwift_race_predictor.py
+++ zwift_race_predictor.py
@@ -10,15 +10,28 @@ class ZwiftRacePredictor:
         self.ws.login()
         self.rider = pd.DataFrame
 
-    def get_race_signups(self, race_id):
+    def get_race_data(self, race_id):
 
         # get signup data from zwiftpower
         raw_data = self.ws.get_page("https://zwiftpower.com/events.php?zid=" + str(race_id))
 
         # format raw data
-        formated_data = BeautifulSoup(raw_data, "lxml")
+        return BeautifulSoup(raw_data, "lxml")
 
-        # extract signup table from formated data
+    def get_rider_data(self, rider_id):
+
+        # get rider data from zwiftpower
+        raw_data = self.ws.get_page("https://zwiftpower.com/profile.php?z=" + str(rider_id))
+
+        # format raw data
+        return BeautifulSoup(raw_data, "lxml")
+
+    def process_race_signups(self, race_id):
+
+        # get race data
+        formated_data = self.get_race_data(race_id)
+
+        # extract signup table from race data
         signups = formated_data.find('table', {'id':'table_event_signups'})
 
         # split signup table into rows, creating the field (array of riders)
@@ -42,14 +55,26 @@ class ZwiftRacePredictor:
         # coverting list into dataframe for easier handling
         self.rider = pd.DataFrame(rider_list, columns=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"])
 
-    def get_race_results(self, race_id):
+    def process_race_results(self, race_id):
+
+        # get race data
+        formated_data = self.get_race_data(race_id)
+
         # TODO write code to extract race results
+        print("nothing implemented yet :(")
+
+    def process_rider_data(self, rider_id):
+
+        # get rider data
+        self.get_rider_data(rider_id)
+
+        # TODO write code to extract rider attributes
         print("nothing implemented yet :(")
 
     def analyze_pre_race(self, race_id):
 
         # get race signup data
-        self.get_race_signups(race_id)
+        self.process_race_signups(race_id)
 
         # TODO write code to analyze race results
         print("nothing implemented yet :(")
@@ -57,7 +82,7 @@ class ZwiftRacePredictor:
     def analyze_post_race(self, race_id):
         
         # get race result data
-        self.get_race_results(race_id)
+        self.process_race_results(race_id)
 
         # TODO write code to analyze race results
         print("nothing implemented yet :(")
