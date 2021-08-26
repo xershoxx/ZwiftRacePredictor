e9cf48458a5c5d59f98519d75199a40a46273c73
Claudio Homeyer <xershoxx@users.noreply.github.com>
2021-08-26 21:47:30 +0200

    structured/created zrp funtions for pre/post

--- zwift_race_predictor.py
+++ zwift_race_predictor.py
@@ -11,6 +11,7 @@ class ZwiftRacePredictor:
         self.rider = pd.DataFrame
 
     def get_race_signups(self, race_id):
+
         # get signup data from zwiftpower
         raw_data = self.ws.get_page("https://zwiftpower.com/events.php?zid=" + str(race_id))
 
@@ -41,10 +42,25 @@ class ZwiftRacePredictor:
         # coverting list into dataframe for easier handling
         self.rider = pd.DataFrame(rider_list, columns=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"])
 
-    def analyze_race(self, race_id):
+    def get_race_results(self, race_id):
+        # TODO write code to extract race results
+        print("nothing implemented yet :(")
+
+    def analyze_pre_race(self, race_id):
+
+        # get race signup data
         self.get_race_signups(race_id)
-        print(self.rider.head())
+
+        # TODO write code to analyze race results
+        print("nothing implemented yet :(")
+
+    def analyze_post_race(self, race_id):
+        
+        # get race result data
+        self.get_race_results(race_id)
+
+        # TODO write code to analyze race results
+        print("nothing implemented yet :(")
 
 zrp = ZwiftRacePredictor()
-
-zrp.analyze_race("2256709")
+zrp.analyze_pre_race("2256709")
